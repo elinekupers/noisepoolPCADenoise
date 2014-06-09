f082ae60b2b0c2bd95c72c40362d48524053075d
helenaXwang <helenawster@gmail.com>
2014-06-09 13:44:57 -0400

    updated megGetOrigChannel.m

--- experiments/megOnOff/megGetOrigChannel.m
+++ experiments/megOnOff/megGetOrigChannel.m
@@ -1,7 +1,20 @@
-function chanNum0 = megGetOrigChannel(chanNum,badChannels)
-% figure out index in the vector with badChannels discarded
+function chanNum0 = megGetOrigChannel(chanNum,badChannels,orig2new)
+% mapping between channel indices between original channel space and the
+% new space with bad channels discarded
+if notDefined('orig2new'), orig2new = true; end
 
-tmp = zeros(1,157);
-tmp(chanNum)=1;
-chanNum0= find(tmp(~badChannels));
-
+if orig2new %figure out index in the vector with badChannels discarded
+    chanNum0 = zeros(size(chanNum));
+    for jj = 1:length(chanNum)
+        tmp = zeros(1,157);
+        tmp(chanNum(jj))=1;
+        chanNum0(jj)= find(tmp(~badChannels));
+    end
+else % figure out index in original space
+    tmp = zeros(1,157);
+    tmp(~badChannels) = 1:sum(~badChannels);
+    chanNum0 = zeros(size(chanNum));
+    for jj = 1:length(chanNum)
+        chanNum0(jj) = find(tmp==chanNum(jj));
+    end
+end
