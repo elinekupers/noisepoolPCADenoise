9b2d93a432c94766506ddd960520b8ceb0c73fb1
Jonathan Winawer <jonathan.winawer@nyu.edu>
2015-03-03 16:07:36 -0500

    neuromag306 layout added to average across channels to replace bad epoch

--- experiments/megOnOff/megReplaceBadEpochs.m
+++ experiments/megOnOff/megReplaceBadEpochs.m
@@ -49,6 +49,49 @@ switch lower(montageType)
         end
         % reshape 2d sensorDataOut into 3d data matrix (chan x time x epoch)
         sensorDataOut = reshape(sensorDataOut, sz(1), sz(2), []);
+    
+    case {'neuromag306'}
+        
+        % we expect 204 gradiometer channels
+        assert(sz(1) == 204);
+        
+        % load the xyz positions of the sensor
+%         net=load('defaultSphereNet.mat');
+        
+        % calculate a distance matrix
+        distances = squareform(pdist(net.xyz), 'tomatrix');
+        
+        % for each channel, find neighbors within X mm
+        nearest = distances < dist & distances > 0;
+        
+        % the sum of the columns should be one
+        nearest = bsxfun(@rdivide, nearest, sum(nearest));
+        
+        % eliminate any NaNs (channels with no neighbors)
+        nearest(isnan(nearest)) = 0;
+        
+        % reshape 3d sensor data into matrix (2d) in order to multiply
+        sensorDataIn = reshape(sensorDataIn,  sz(1), []);
+        
+        % Initialize out data
+        sensorDataOut = sensorDataIn;
+        
+        
+        % Replace bad channels with the mean of the neighbors
+        
+        for jj = 1:204
+            sprintf('%d',jj)
+            inds = find(nearest(:, jj));
+            %if jj <= 3, disp(length(inds)); end
+            badPoints = isnan(sensorDataOut(jj,:));
+            tmp = sensorDataIn(inds,badPoints);
+            tmp(isnan(tmp)) = 0;
+            sensorDataOut(jj,badPoints) = (tmp' * nearest(inds, jj))';
+        end
+        % reshape 2d sensorDataOut into 3d data matrix (chan x time x epoch)
+        sensorDataOut = reshape(sensorDataOut, sz(1), sz(2), []);
+        
+        
         
     otherwise
         error('Unknown montage type %s', montageType);
