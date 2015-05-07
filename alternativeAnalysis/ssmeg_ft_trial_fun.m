bce5152021feeb233b4a6c066427b0957af62a00
Jonathan Winawer <jonathan.winawer@nyu.edu>
2015-05-07 16:35:15 -0400

    ica analysis fixes

--- alternativeAnalysis/ssmeg_ft_trial_fun.m
+++ alternativeAnalysis/ssmeg_ft_trial_fun.m
@@ -1,23 +1,25 @@
-function [trl, event] = ssmeg_ft_trial_fun(cfg)
+function [trl, Events] = ssmeg_ft_trial_fun(cfg, onsets, conditions)
 
 % read the header information and the events from the data
 hdr   = ft_read_header(cfg.dataset);
-event = cfg.event;
 
-% search for "trigger" events
-sample = [cfg.event(find(strcmp('trigger', {cfg.event.type}))).sample]';
+% Get onset of trigger events
+trigger = find(onsets);
 
 % determine the number of samples before and after the trigger
 pretrig  = -round(cfg.trialdef.pre  * hdr.Fs);
 posttrig =  round(cfg.trialdef.post * hdr.Fs);
 
-% look for the combination of a trigger "7" followed by a trigger "64" 
-% for each trigger except the last one
-trl = [];
-for j = 1:(length(value))
-    trlbegin = sample(j) + pretrig;
-    trlend   = sample(j) + posttrig;
-    offset   = pretrig;
-    newtrl   = [trlbegin trlend offset];
-    trl      = [trl; newtrl];
+for j = 1:length(trigger);  
+      
+    trlbegin = trigger(j) +  pretrig;     
+    trlend   = trigger(j) + posttrig;       
+    trl(j,:) = [trlbegin trlend pretrig];
+    
+    Events(j).trigger = trigger(j);
+%     Events(j).channel = trigger(j).type; % I don't think we need this..
+    Events(j).value   = conditions(j);
 end
+
+
+return
