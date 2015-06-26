6723598e6244e4712d9279850107737b3fceb5e2
Jonathan Winawer <jonathan.winawer@nyu.edu>
2015-06-26 14:14:26 -0400

    cleane

--- exampleAnalysis/denoise_subfunctions/megGetSLandABfrequencies.m
+++ exampleAnalysis/denoise_subfunctions/megGetSLandABfrequencies.m
@@ -1,4 +1,4 @@
-function freq = megGetSLandABfrequencies(f, T, slF)
+function freq = megGetSLandABfrequencies(f, slF, lf_cutoff, tol)
 % Get the stimulus-locked (sl_f), asynchronous broadband (ab_f), and keep
 % (keep_f) frequencies and their indices (sl_i, ab_i, keep_i) into f
 % (frequency vector). Broadband frequencies are all frequencies between min
@@ -10,55 +10,61 @@ function freq = megGetSLandABfrequencies(f, T, slF)
 %
 % Inputs:
 %   f: vector of frequencies
-%   T: epoch length (should be equal to 1/f(2))
 %   slF: stimulus-locked frequency (twice the stimulus flicker rate)
+%   lf_cutoff: exclude frequencies below this value
+%   tol: exlude frequencies within tol of requested frequencies
+%
 % Output:
 %   freq: structure containing the fields 'all', 'ab', 'sl', 'keep', 
 %         'ab_i', 'sl_i', 'keep_i'
+%
+% Example:
+%   freq = megGetSLandABfrequencies(0:199, 12, 60, 1.5);
 
 %% Check Inputs
 
-% epoch length
-if notDefined('T'), T = 1/f(2); end
-
 % stimulus locked frequency
-if notDefined('slF'), slF = 12/T; end
+if notDefined('slF'),       slF = [];       end
+if notDefined('tol'),       tol = 1.5;      end
+if notDefined('lf_cutoff'), lf_cutoff = 10; end
 
-%% stimulus locked (2nd harmonic only).
-% Should be same as slF, but this ensures that it is exactly a value in f
-[~, sl_i] = min(abs(f - slF));
-sl_f = f(sl_i);
+%% Asyncrhonous Broadband
+% Exclude all frequencies that are close to a multiple of the
+% stimulus-locked frequency
+if isempty(slF), 
+    sl_drop = []; 
+else
+    sl_drop  = f(mod(f, slF) <= tol | mod(f, slF) > slF - tol);
+   
+end
 
-%% Asyncrhonous Broadband - take all frequencies except: 
-% (a) stimulus locked and harmonics,
-% (b) line noise and harmonics
-% (c) low frequencies (up to 7 Hz)
+% Exclude all frequencies that are close to a multiple of the
+% line noise frequency
+ln_drop   = f(mod(f, 60) <= tol | mod(f, 60) > 60 - tol);
 
-% drop the stimulus-locked frequncy and harmonics (frequencies within 3 Hz
-% of multiples of the SL frequency, close to 15 Hz)
-tmp     = (1:12)*slF;
-%sl_drop  = sort([tmp-3 tmp-2 tmp-1 tmp tmp+1 tmp+2 tmp+3]);
-sl_drop  = sort([tmp-1 tmp tmp+1]);
+% Exclude all frequencies below lf_cutoff
+lf_drop = f(f<lf_cutoff);
 
-% drop line noise(frequencies within 3 Hz of multiples of 60 Hz)
-tmp = (1:5) * 60;
-%ln_drop   = sort([tmp-3 tmp-2 tmp-1 tmp tmp+1 tmp+2 tmp+3]);
-ln_drop   = sort([tmp-1 tmp tmp+1]);
+% Define the frequenies and indices into the frequencies used to compute
+% broadband power
+[ab_f, ab_i]   = setdiff(f, [sl_drop ln_drop lf_drop]);
 
-% low frequency drop
-lf_drop = 0:60;
+%% Stimulus locked
+if isempty(slF)
+    sl_f = []; sl_i = [];
+else
+    [~, sl_i] = min(abs(f-slF)); sl_f = f(sl_i);
+end
 
-[~, ab_i]   = setdiff(round(f), [sl_drop ln_drop lf_drop]);
-ab_f = f(ab_i);
-
-%% keep frequencies - drop line noise and low frequencies, keep the rest
-[~, keep_i]   = setdiff(round(f*T), [ln_drop lf_drop]);
-keep_f = f(keep_i);
+%% keep frequencies - for plotting
+%   drop line noise and low frequencies, keep the rest (including those
+%               used for broadband and stimulus locked compuation)
+[keep_f, keep_i]   = setdiff(f, [ln_drop lf_drop]);
 
 %% Return a struct containing frequencies and indices into these frequencies
 freq.all    = f;        % all frequencies
 freq.ab     = ab_f;     % frequencies used for computing broadband response
-freq.sl     = sl_f;     % stimulus locked frequency (2*stimulus flicker rate)
+freq.sl     = sl_f;     % stimulus locked frequency 
 freq.keep   = keep_f;   % all frequencies between min and max excluding line noise
 freq.ab_i   = ab_i;     % index into f.all of ab frequencies
 freq.sl_i   = sl_i;     % index into f.all of sl frequencies
