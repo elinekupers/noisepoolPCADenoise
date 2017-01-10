e68e8413867b43cfc40c9fc58aea954778b0b65b
elinekupers <elinekupers@gmail.com>
2017-01-10 15:15:01 -0500

    make sure color order corresponds with data order

--- analysis/denoise_subfunctions/dfdGetColors.m
+++ analysis/denoise_subfunctions/dfdGetColors.m
@@ -6,15 +6,15 @@ function colors = dfdGetColors(nrColors)
 % 
 % AUTHORS. TITLE. JOURNAL. YEAR.
 %
-% This function will return a matrix with either 3 RGB color codes (blue, red,
-% green) or 4 RGB colors codes (blue, red, green and grey). Corresponding
-% to conditions Full, Right and Left (Blank).
+% This function will return a matrix with either 3 RGB color codes (blue, green, red) 
+% or 4 RGB colors codes (blue, green, red, and grey). Corresponding
+% to conditions Full,  Left and right (Blank).
 
 switch nrColors
     case 3
-        colors = [63, 121, 204; 228, 65, 69; 116,183,74]/255;
+        colors = [63, 121, 204;  116,183,74; 228, 65, 69;]/255;
     case 4
-        colors = [63, 121, 204; 228, 65, 69; 116,183,74; 127,127,127]/255;
+        colors = [63, 121, 204; 116,183,74; 228, 65, 69; 127,127,127]/255;
         
 end
 
