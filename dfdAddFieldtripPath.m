f6a7240fb897d3c97017b2ff0d80ea898a75eb0a
elinekupers <elinekupers@gmail.com>
2017-03-10 15:49:53 -0500

    deal with multiple mounts of server

--- dfdAddFieldtripPath.m
+++ dfdAddFieldtripPath.m
@@ -1,5 +1,6 @@
 function dfdAddFieldtripPath(path)
-    if notDefined('path'), path = '/Volumes/server/Projects/MEG/code/fieldtrip'; end
+    if notDefined('path'), path = '/Volumes/server/Projects/MEG/code/fieldtrip'; 
+    if ~exist(path,'dir'); path = '/Volumes/server-1/Projects/MEG/code/fieldtrip'; end; end
     addpath(path);
     ft_defaults;
 return
