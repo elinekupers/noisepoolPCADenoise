8b7ed28d61297d258c5424a72540899e17ecd1e7
Jonathan Winawer <jonathan.winawer@nyu.edu>
2017-11-15 10:39:58 -0500

    Eliminate dependency on our lab-specific paths

--- dfdAddFieldtripPath.m
+++ dfdAddFieldtripPath.m
@@ -1,6 +1,10 @@
-function dfdAddFieldtripPath(path)
-    if notDefined('path'), path = '/Volumes/server/Projects/MEG/code/fieldtrip'; 
-    if ~exist(path,'dir'); path = '/Volumes/server-1/Projects/MEG/code/fieldtrip'; end; end
-    addpath(path);
+function dfdAddFieldtripPath(pth)
+% Add fieldtrip to Matlab path
+% dfdAddFieldtripPath(pth)
+%
+% Example: 
+%   dfdAddFieldtripPath('~/matlab/toolboxes/Fieldtrip/')
+
+    addpath(pth);
     ft_defaults;
 return
