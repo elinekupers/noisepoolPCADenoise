0439bac1239716fe2a269859b99374d526719d63
elinekupers <elinekupers@gmail.com>
2017-12-13 10:51:24 -0500

    add comments to figure script s3

--- analysis/figure_scripts/dfdMakeFigureS3.m
+++ analysis/figure_scripts/dfdMakeFigureS3.m
@@ -19,19 +19,24 @@ saveFigures     = 1;
 
 for whichSubject    = 1%:8;
     
+    %% Panel A
+
+    % Load data
     load(fullfile(dataDir,sprintf('S0%d_noisedata.mat',whichSubject)));
     
+    % Define colors based on number of epochs
     cmap = copper(size(noisedata,3));
     
     % Plot the variance explained by the PCs of the noise data
-    
     explained = zeros(size(noisedata,1),size(noisedata,3));
     
+    % Compute PCA on noise timeseries and keep the variance explained by
+    % PCs
     for ii = 1:size(noisedata,3)
         [coeff, score, latent, tsquared, explained(:,ii)] = pca(noisedata(:,:,ii)');
     end
     
-    
+    % Plot variance explained by PCs
     figure; hold on;
     for ii = 1:size(noisedata,3); plot(explained(:,ii), 'Color', cmap(ii,:)); end
     plot(mean(explained,2),'k--','LineWidth',2)
@@ -43,6 +48,7 @@ for whichSubject    = 1%:8;
         figurewrite(fullfile(figureDir,'figureVarExpl'),[],0,'.',1);
     end
     
+    % Plot inset of variance explained by PCs
     figure; hold all;
     for ii = 1:size(noisedata,3); plot(explained(:,ii), 'Color', cmap(ii,:)); end
     plot(mean(explained,2),'k--','LineWidth',2);
@@ -55,17 +61,22 @@ for whichSubject    = 1%:8;
         figurewrite(fullfile(figureDir,'figureVarExpl_inset'),[],0,'.',1);
     end
     
-    % Plot spectrum of the noise data pcs
+    %% Panel B 
+    
+    % Load PC noise time series
     load(fullfile(dataDir,sprintf('S0%d_noisepcs.mat',whichSubject)));
     
-    figure; hold all;
+   
+    % Get amplitudes in Fourier space
     for ii = 1:size(pcs,1)
         amps(ii,:,:) = abs(fft(pcs{ii}(:,1:10)));
-        %         plot(0:999,squeeze(amps(ii,:,:)));
     end
     
+     % Plot spectrum of the noise data pcs
+    figure; hold all;
     for n_pcs = 1:10
-        plot(1:1000, squeeze(mean(amps(:,:,n_pcs),1)),'k','LineWidth',1); end
+        plot(1:1000, squeeze(mean(amps(:,:,n_pcs),1)),'k','LineWidth',1);
+    end
     plot(1:1000, squeeze(mean(mean(amps,1),3)),'r','LineWidth',2);
     
     xlim([60 150]); ylim(10.^[1.70, 2.1]); set(gca,'XScale','log','YScale','log');
@@ -78,14 +89,16 @@ for whichSubject    = 1%:8;
     end
     
     
-    %% Or take one channel and compute the envelope
+    %% Panel C and D
     
+    % Take one epoch from one PC and compute the envelope
     thispc = 1;
     thisepoch = 10;
     
-    
     thisnoise = pcs{thisepoch}(:,thispc);
     envelope = hilbert(thisnoise);
+    
+    % Plot time series and envelope
     figure; plot(0:999,thisnoise); hold on;
     plot(0:999,abs(envelope));
     ylim([-4 4]);
@@ -100,6 +113,7 @@ for whichSubject    = 1%:8;
         figurewrite(fullfile(figureDir,sprintf('figureNoiseData_pc%d_epoch%d',thispc,thisepoch)),[],0,'.',1);
     end
     
+    % Plot amplitude spectrum of envelope
     figure; plot(0:999, abs(fft(abs(envelope))));
     xlim([0 150]);  ylim([0 1000]); xlabel('Frequency (Hz)'); ylabel('Amplitude');
     title('Fourier transform of one epoch time series envelope')
@@ -109,10 +123,9 @@ for whichSubject    = 1%:8;
         figurewrite(fullfile(figureDir,sprintf('figureNoiseDataEnvelope_pc%d_epoch%d',thispc, thisepoch)),[],0,'.',1);
     end
     
+    % And make an inset
     figure; plot(0:999, abs(fft(abs(envelope))));
-%     ylim([0 275]);
-    
-    xlim([0 150]); xlabel('Frequency (Hz)'); ylabel('Amplitude');
+    xlim([0 150]); ylim([0 275]); xlabel('Frequency (Hz)'); ylabel('Amplitude');
     title('Fourier transform of one epoch time series envelope')
     makeprettyaxes(gca,9,9);
     
@@ -120,13 +133,9 @@ for whichSubject    = 1%:8;
         figurewrite(fullfile(figureDir,sprintf('figureNoiseDataEnvelope_pc%d_epoch%d_inset',thispc, thisepoch)),[],0,'.',1);
     end
     
-            
-  
-    
-    
-    
-    %% Or the mean of envelopes across epochs
-    
+    %% PANEL E 
+
+    % Compute the mean of envelopes across epochs
     envelope =  zeros(size(pcs{1},1),10,length(pcs));
     for ii = 1:867
         for jj = 1:10
